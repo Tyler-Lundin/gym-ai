32cdbac1b93e8ba27c877c64a418649dd8122eb0
Tyler Lundin <tylerjameslundin@gmail.com>
2025-02-10 03:02:32 -0800

    Remove unused import

--- app/(components)/landing-page.tsx
+++ app/(components)/landing-page.tsx
@@ -1,4 +1,3 @@
-import { useClerk } from "@clerk/clerk-react";
 import { currentUser } from "@clerk/nextjs/server";
 import { redirect } from "next/navigation";
 import { RiArrowRightLine, RiHexagonFill } from "react-icons/ri";
